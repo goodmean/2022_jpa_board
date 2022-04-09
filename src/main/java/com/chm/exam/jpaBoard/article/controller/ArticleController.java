ede57ad629f9964892e87bfaac45cb6360b47632
goodmean <zizigoo12@gmail.com>
2022-04-09 15:41:17 +0900

    타임리프 추가, list.html 파일추가

--- src/main/java/com/chm/exam/jpaBoard/article/controller/ArticleController.java
+++ src/main/java/com/chm/exam/jpaBoard/article/controller/ArticleController.java
@@ -28,22 +28,9 @@ public class ArticleController {
 //	}
 
 	@RequestMapping("list")
-	@ResponseBody
 	public String showList() {
-		List<Article> articles = articleRepository.findAll();
-		
-		String html = "";
-		
-		html += "<ul>";
 
-		for(Article article : articles){
-			html += "<li>";
-			html += "%d번 / %s / %s".formatted(article.getId(), article.getTitle(), article.getBody());
-			html += "</li>";
-		}
-		html += "</ul>";
-		
-		return html;
+		return "usr/article/list";
 	}
 
 	@RequestMapping("list2")
